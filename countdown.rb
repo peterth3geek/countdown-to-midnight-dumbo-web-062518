d91c3cd75f59929edbe8f77825a335906736eddf
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-20 17:33:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(int)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  countdown = int
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
