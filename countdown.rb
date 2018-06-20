44c1757e0dfb64869bf7b4b09a25f52b12312d41
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-20 17:29:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  countdown = integer
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+  
 end
